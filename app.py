7a71b3361a7e80ee6d8b4b0631bccad26df79c03
Jeremy Roy <jezoffy@gmail.com>
2022-02-19 15:14:50 +0000

    app.py update

--- app.py
+++ app.py
@@ -2,14 +2,9 @@
 Simple hello world
 """
 
-from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
-from datetime import datetime
 
 app = Flask(__name__)
 

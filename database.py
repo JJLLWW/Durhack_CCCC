1e06ae93aa0c03e8746251197fa62cde815f7142
osps05122002 <92094991+osps05122002@users.noreply.github.com>
2022-02-19 15:26:25 +0000

    verify user added

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+from ast import Pass
+from collections import namedtuple
 import sqlite3
 import database2 as db2
 
@@ -17,10 +19,21 @@ class user:
         self.all = [self.id, self.name, self.Pass, self.langid, self.email]
 
 def add_user(Id, Name, Pass, Langid, Email):
+    exist_name = ""
     cursor.execute("""
+        SELECT NAME FROM USER where NAME = ?
+    """, [Name])
+    for row in cursor:
+        exist_name = (row[0])
+    if exist_name != "":
+        cursor.execute("""
         INSERT INTO USER (USERID, NAME, PASSWORD, LANGUAGEID, EMAIL) VALUES(?, ?, ?, ?, ?)
-    """, [Id, Name, Pass, Langid, Email])  
+        """, [Id, Name, Pass, Langid, Email])  
+        print("Done")
+    else:
+        print("already exists")
     
+#add_user(234, "Olivia", "olivia", "45456", "@olivia.com")   
 add_user(0, 'name2', 'pasass', 0, 'email.com')
 add_user(0, 'name', 'pasass2', 0, 'email.com')
 
@@ -33,6 +46,31 @@ def get_user(name):
         newusr = user(row[0], row[1], row[2], row[3], row[4])
         return newusr
 
-usr = get_user("name2")
-print(usr.all)
+def verify_user(name, password):
+    exist_name = ""
+    real_password = ""
+    cursor.execute("""
+    SELECT NAME FROM USER where NAME = ?
+    """, [name])
+    for row in cursor:
+        print("in")
+        exist_name = (row[0])
+    #print(exist_name)
+    if exist_name != "":
+        print("exist")
+        cursor.execute("""
+        SELECT NAME FROM USER where PASSWORD = ?
+        """, [password])
+        print(password)
+    else:
+        return False
+        print("no")
+
+verify_user("name2", "pasass")
+
+
+#usr = get_user("name2")
+#print(usr.all)
+
+print("hiiiiiiii")
 

2795c79e1dc426575d6387fa1400782f6cd7274f
Jack Wright <jack.louie.wright@gmail.com>
2022-02-19 19:33:34 +0000

    flask socket hook

--- app.py
+++ app.py
@@ -2,12 +2,21 @@
 Simple hello world
 """
 
+from socket import SocketIO, socket
 from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_socketio import SocketIO
 from flask_session import Session
 import datetime
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/", methods=["GET","POST"])
 def hello_world():
     return render_template("chat.html")
+
+@socketio.on('join_chat')
+def test_join():
+    print("Ysadbadj")
+
+app.run()

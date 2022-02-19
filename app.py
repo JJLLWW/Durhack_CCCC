221b0fe9f45b6c87f67e9c275550d170e534002a
Jeremy Roy <jezoffy@gmail.com>
2022-02-19 15:14:09 +0000

    app.py update

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ Simple hello world
 """
 
 from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html")

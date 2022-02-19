e3dc993feac9e7e2b14b8a6b21572ed60f356ed7
osps05122002 <92094991+osps05122002@users.noreply.github.com>
2022-02-19 19:27:38 +0000

    Complete SQL functionality

--- database.py
+++ database.py
@@ -4,54 +4,93 @@ import sqlite3
 
 def create_tables(cursor):
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS USER
-            (USERID INTEGER,
-            NAME TEXT,
+    CREATE TABLE IF NOT EXISTS U
+            (NAME TEXT,
             PASSWORD TEXT,
             LANGUAGEID INTEGER,
             EMAIL TEXT);
     """)
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS MESSAGES
-            (MESSAGEID INTEGER,
-            PASSWORD TEXT,
-            LANGUAGEID INTEGER,
-            EMAIL TEXT);
+    CREATE TABLE IF NOT EXISTS M
+            (SENDER TEXT,
+            RECEIVER TEXT,
+            MESSAGE TEXT,
+            NUMBER INTEGER PRIMARY KEY AUTOINCREMENT);
     """)
 
+
 class user:
-    def __init__(self, id, name, password, langid, email):
-        self.id = id
+    def __init__(self, name, password, langid, email):
         self.name = name
         self.Pass = password
         self.langid = langid
         self.email = email
-        self.all = [self.id, self.name, self.Pass, self.langid, self.email]
+        self.all = [self.name, self.Pass, self.langid, self.email]
 
-def add_user(Id, Name, Pass, Langid, Email, cursor):
+def add_user(Name, Pass, Langid, Email, cursor):
+    exist_name = ""
     cursor.execute("""
-        SELECT NAME FROM USER where NAME = ?
+        SELECT NAME FROM U where NAME = ?
     """, [Name])
     # if there is an entry already we don't do anything
     for row in cursor:
-        return
-    cursor.execute("""
-    INSERT INTO USER (USERID, NAME, PASSWORD, LANGUAGEID, EMAIL) VALUES(?, ?, ?, ?, ?)
-    """, [Id, Name, Pass, Langid, Email])  
+        # if the name exists, check the password
+        exist_name = (row)
+    if exist_name == "":
+        cursor.execute("""
+        INSERT INTO U (NAME, PASSWORD, LANGUAGEID, EMAIL) VALUES(?, ?, ?, ?)
+        """, [Name, Pass, Langid, Email])  
+    else:
+        print("user already exists")
 
 #add_user(234, "Olivia", "olivia", "45456", "@olivia.com")   
 
-
+def add_message(user1, user2, text, cursor):
+    cursor.execute("""
+    INSERT INTO M (SENDER, RECEIVER, MESSAGE) VALUES(?, ?, ?)
+    """, [user1, user2, text])
 
 def get_user(name, cursor):
     cursor.execute("""
-        SELECT * FROM USER where NAME = ?
+        SELECT * FROM U where NAME = ?
     """, [name])
     result = ""
     for row in cursor:
-        newusr = user(row[0], row[1], row[2], row[3], row[4])
+        newusr = user(row[0], row[1], row[2], row[3])
         return newusr
+
+def get_message(user1, user2, cursor):
+    cursor.execute(""" 
+    SELECT NUMBER FROM M where (SENDER,RECEIVER) = (?, ?) 
+    """, [user1, user2])
+    result = []
+    for row in cursor:
+        result.append(row)
+
+    cursor.execute(""" 
+    SELECT NUMBER FROM M where (RECEIVER, SENDER) = (?, ?) 
+    """, [user1, user2])
+    for row in cursor:
+        result.append(row)
+    res=[]
+    for i in result:
+        res.append(i[0])
+
+    res.sort()
+    print(res,"result")
+    conversation = []
+
+    for num in res:
+        cursor.execute(""" 
+        SELECT SENDER, RECEIVER, MESSAGE FROM M where (NUMBER) = (?) 
+        """, [num])
+        for row in cursor:
+           conversation.append(row)
+
+    print(conversation)
+
+
 
 SUCCESS, ERR_NOUSR, ERR_WRONGPASS = 0, 1, 2
 def errmsg_from_code(code):
@@ -65,15 +104,13 @@ def errmsg_from_code(code):
 def verify_user(name, password, cursor):
     real_password = None
     cursor.execute("""
-        SELECT NAME FROM USER where NAME = ?
+        SELECT NAME FROM U where NAME = ?
     """, [name])
     print(name)
     for row in cursor:
         # if the name exists, check the password
-        print("exist")
-        exist_name = (row)
         cursor.execute("""
-        SELECT PASSWORD FROM USER where NAME = ?
+        SELECT PASSWORD FROM U where NAME = ?
         """, [name])
         for row in cursor:
             real_password = row[0]
@@ -84,6 +121,5 @@ def verify_user(name, password, cursor):
             return ERR_WRONGPASS
     return ERR_NOUSR
 
-#usr = get_user("name2")
-#print(usr.all)
+
 
--- main.py
+++ main.py
@@ -5,9 +5,15 @@ con = sqlite3.connect("database.db")
 cursor = con.cursor()
 db.create_tables(cursor)
 
-db.add_user(0, 'name2', 'pasass', 0, 'email.com', cursor)
-db.add_user(0, 'name', 'pasass2', 0, 'email.com', cursor)
-db.add_user(0, 'name', 'pasass2', 0, 'email.com', cursor)
+db.add_user( 'name2', 'pasass', 0, 'email.com', cursor)
+db.add_user( 'name', 'pasass2', 0, 'email.com', cursor)
+db.add_user( 'name', 'pasass2', 0, 'email.com', cursor)
+
+db.add_message("user2", "user1", "text", cursor)
+db.add_message("user1", "user2", "texts", cursor)
+db.add_message("user1", "user2", "thi", cursor)
+db.add_message("user2", "user1", "tfejn", cursor)
+db.get_message("user1", "user2", cursor)
 
 usr = db.get_user('name', cursor)
 print(usr.all)

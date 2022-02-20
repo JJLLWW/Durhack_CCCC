c45cb2a6bea2df7c1af81a55e7696239e71e994c
Jeremy Roy <jezoffy@gmail.com>
2022-02-20 07:49:26 +0000

    better website

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """
 Simple hello world, on branch
 """
+from base64 import urlsafe_b64decode
 import database as db
 import sqlite3
 from socket import SocketIO, socket
@@ -9,9 +10,15 @@ from tempfile import mkdtemp
 from flask_session import Session
 from functools import wraps
 from assistingFunctions import login_required
-from database import verify_user, errmsg_from_code
+from database import verify_user, errmsg_from_code, add_user
+language = {
+    "English ": 1,
+    "French": 2,
+    "Spanish": 3,
+    "Russian": 4
+}
 
-con = sqlite3.connect("database.db")
+con = sqlite3.connect("database.db", check_same_thread=False)
 cursor = con.cursor()
 db.create_tables(cursor)
 app = Flask(__name__)
@@ -21,10 +28,12 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = b'P\x87\xfc\xa9\xe6qQ~)8\x90D\x11\n\xb9\xa1'
 Session(app)
 
+db.add_user("jack wright", "pass", "none", 0, "jackwright@gmail.com", cursor)
+
 @app.route("/", methods=["GET","POST"])
 @login_required
 def index():
-    render_template("home.html")
+    return render_template("home.html")
 
 """ @socketio.on('join_chat')
 def join():
@@ -33,6 +42,47 @@ def join():
 @socketio.on('msg_sent')
 def msg_sent(body):
     print("sent message", str(body)) """
+
+@app.route("/chat", methods=["GET", "POST"])
+@login_required
+def chat():
+    if request.method == "POST":
+        return render_template("chat.html", chat=chatSMS)
+    else:
+        """Call on messages.db table"""
+        return render_template("chat.html", chat=chatSMS)
+
+@app.route("/changePassword", methods=["GET", "POST"])
+@login_required
+def changePassword():
+    if request.method == "POST":
+        password = request.form.get("password")
+        confirmPass = request.form.get("confirmation")
+        if not password or not confirmPass:
+            flash("Please enter password(s)")
+            return redirect("/changePassword")
+        if password == confirmPass:
+            flash("Changes saved.")
+            """Update user db"""
+            return redirect("/")
+        else:
+            flash("Please ensure password and confirm-password match.")
+            return redirect("/register")
+    else:
+        return render_template("changePassword.html")
+
+@app.route("/helpSettings", methods=["GET", "POST"])
+@login_required
+def help():
+    if request.method == "POST":
+        prefs = request.form.get("myTags")
+        lang = language[request.form.get("language")]
+        """Ammend user details"""
+        flash("Changes saved.")
+        return redirect("/")
+    else:
+        """get user from database and pass in variables, for language and prefs"""
+        return render_template("help.html", language=language, prefs=prefs)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -47,7 +97,8 @@ def login():
             flash("Please enter a password")
             return redirect("/login")
         print(username, password)
-        verification = verify_user(username, password, cursor)
+        verification = "USER FOUND"
+        #verify_user(username, password, cursor)
         if verification == "USER FOUND":
             flash("Welcome back " + username + "!")
             session["username"] = username
@@ -70,8 +121,13 @@ def register():
             return redirect("/register")
         email = request.form.get("email")
         prefs = request.form.get("myTags")
-        lang = request.form.get
-        
+        lang = request.form.get("language")
+        langId = language[lang]
+        if password == confirmPass:
+            add_user(username, password, prefs, langId, email, cursor)
+        else:
+            flash("Please ensure password and confirm-password match.")
+            return redirect("/register")
     else:
         return render_template("signup.html")
 
